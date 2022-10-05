89662031ec0048f7cf4a5b599ce1cbd8fe44950c
winaldolaua <winaldodwiputra@gmail.com>
2022-10-06 00:29:53 +0700

    Create profile.php

---
+++ profile.php
@@ -0,0 +1,10 @@
+<div class="profile">
+    <img src="assets/img/me.jpeg" alt="" class="img-fluid rounded-circle">
+    <h1 class="text-light"><a href="index.html">Winaldo D.P.L</a></h1>
+    <div class="social-links mt-3 text-center">
+        <a href="#" target="_blank" class="twitter"><i class="bx bxl-twitter"></i></a>
+        <a href="#" target="_blank" class="facebook"><i class="bx bxl-facebook"></i></a>
+        <a href="https://www.instagram.com/winaldoputra/?hl=id" target="_blank" class="instagram"><i class="bx bxl-instagram"></i></a>
+       
+    </div>
+</div>
