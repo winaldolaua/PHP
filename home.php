14a34d559278416553d0418abf9838122d2a6543
winaldolaua <winaldodwiputra@gmail.com>
2022-10-06 00:29:47 +0700

    Create home.php

---
+++ home.php
@@ -0,0 +1,6 @@
+<section id="hero" class="d-flex flex-column justify-content-center align-items-center">
+    <div class="hero-container" data-aos="fade-in">
+        <h1>Winaldo D.P.L</h1>
+        <p>Saya <span class="typed" data-typed-items="Mahasiswa, Pecinta Musik, Penggemar Olahraga"></span></p>
+    </div>
+</section>
