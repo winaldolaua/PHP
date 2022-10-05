d98106da2296110768c41031292487a5b403dfae
winaldolaua <winaldodwiputra@gmail.com>
2022-10-06 00:29:58 +0700

    Create tasks.php

---
+++ tasks.php
@@ -0,0 +1,37 @@
+<section id="task">
+    <div class="container">
+    <div class="section-title">
+            <h2>Task</h2>
+        </div>
+
+        <div class="row">
+            <div class="card" style="width: 18rem;">
+                <div class="card-body"  align="center">
+                    <h5 class="card-title">Javascript</h5>
+                    <a href="https://github.com/winaldolaua/javascript" class="card-link">Tugas 1 Javascript</a>
+                    <a href="https://github.com/winaldolaua/javascript/tree/tugas-2-javascript" class="card-link">Tugas 2 Javascript</a>
+                    <a href="https://github.com/winaldolaua/javascript/tree/tugas-3-javascript" class="card-link">Tugas 3 Javascript</a>
+                    <a href="https://github.com/winaldolaua/javascript/tree/tugas-4-javascript" class="card-link">Tugas 4 Javascript</a>
+                    <a href="https://github.com/winaldolaua/javascript/tree/tugas-5-javascript" class="card-link">Tugas 5 Javascript</a>
+                    <a href="https://github.com/winaldolaua/javascript/tree/tugas-6-javascript" class="card-link">Tugas 6 Javascript</a>
+                    <a href="https://github.com/winaldolaua/javascript/tree/tugas-7-javascript" class="card-link">Tugas 7 Javascript</a>
+                    
+            </div>
+            </div>
+
+            <div class="card" style="width: 18rem;">
+                <div class="card-body"  align="center">
+                    <h5 class="card-title">Tugas PHP</h5>
+                     <a href="https://github.com/winaldolaua/PHP/tree/tugas-2-php" class="card-link">Tugas 2 PHP</a>
+                     <a href="https://github.com/winaldolaua/PHP/tree/tugas-3-php" class="card-link">Tugas 3 PHP</a>
+                     <a href="https://github.com/winaldolaua/PHP/tree/tugas-4-php" class="card-link">Tugas 4 PHP</a>
+                     <a href="https://github.com/winaldolaua/PHP/tree/tugas-5-php" class="card-link">Tugas 5 PHP</a>
+                      
+            </div>
+            </div>
+        
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
