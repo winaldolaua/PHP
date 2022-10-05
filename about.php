fb53fe08ffb4afd6913e4802db20160784aef908
winaldolaua <winaldodwiputra@gmail.com>
2022-10-06 00:23:48 +0700

    Create about.php

---
+++ about.php
@@ -0,0 +1,43 @@
+<section id="about" class="about">
+    <div class="container">
+
+        <div class="section-title">
+            <h2>About Me</h2>
+            <p>Nama saya Winaldo Dwi Putra Laua, saya seorang mahasiswa teknik informatika di universitas negeri malang yang sekarang menjalani semester 7. Saya berasal dari kota Palu, Sulawesi Tengah dan sekarang berada di kota Malang.</p>
+        </div>
+
+            <div class="col-lg-8 pt-4 pt-lg-0 content" data-aos="fade-left">
+                <h3>Winaldo Dwi Putra Laua</h3>
+                <p class="fst-italic">
+                Keseharian saya dihabiskan dengan belajar musik, bermain game, belajar dan sesekali olahraga. Saya suka jalan-jalan mencari tempat baru dan juga teman baru ataupun hal-hal baru yang lainnya karena itu membuat diri saya berkembang.
+                Saya berharap setelah saya lulus kuliah saya bisa dapat pekerjaan yang baik dan bisa memiliki kehidupan yang nyaman dan santai.
+                </p>
+                <div class="row">
+                    <div class="col-lg-6">
+                        <ul>
+                            <li><i class="bi bi-chevron-right"></i> <strong>Birthday:</strong> <span>9 May 2001</span>
+                            </li>
+                            
+                            <li><i class="bi bi-chevron-right"></i> <strong>Phone:</strong> <span>+6282335647717</span>
+                            </li>
+                            <li><i class="bi bi-chevron-right"></i> <strong>Domisili</strong> <span>Malang, Indonesia</span>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-lg-6">
+                        <ul>
+                            <li><i class="bi bi-chevron-right"></i> <strong>Age:</strong> <span>21</span></li>
+                            <li><i class="bi bi-chevron-right"></i> <strong>Status</strong> <span>Mahasiswa</span></li>
+                            
+                            </li>
+                            <li><i class="bi bi-chevron-right"></i> <strong>Hobi:</strong> <span>Musik & Olahraga</span>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+
+    </div>
+</section>
