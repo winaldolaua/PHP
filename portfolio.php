63aa6b5b8a99e59f63e6af368815628518fc8d35
winaldolaua <winaldodwiputra@gmail.com>
2022-10-06 00:29:51 +0700

    Create portfolio.php

---
+++ portfolio.php
@@ -0,0 +1,39 @@
+<section id="about" class="about">
+    <div class="container">
+
+        <div class="section-title">
+            <h2>Portfolio</h2>
+        </div>
+
+        <div class="col-lg-7">
+                <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active" data-bs-interval="1000">
+                            <img src="assets/img/Picture1.jpg" class="d-block w-100" alt="...">
+                        </div>
+                        
+                </div>
+            </div>
+
+        <div class="row">
+            <div class="col-lg-8 pt-4 pt-lg-0 content" data-aos="fade-left">
+                <h3>B-air</h3>
+                <p class="fst-italic">
+                   Project website yang dikembangkan untuk keperluan ujian bahasa inggris yang saya buat bersama kelompok dalam rangka kegiatan magang kampus.
+                </p>
+
+                <div class="col-lg-0">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">B-air</h5>
+                        <ul>
+                            <li><strong>Kategori</strong>: Fullstack Web Development</li>
+                            <li><strong>Tempat</strong>: UPT Siskomtek Universitas Negeri Malang</li>
+                            <li><strong>Tanggal pengerjaan</strong>: 06 Mei 2022 - 06 Agustus 2022</li>
+                        </ul>
+                    </div>
+                </div>
+
+
+    </div>
+</section>
