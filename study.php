6ec58f8a206dcbabc968b1af7722d4f128fb4bdc
winaldolaua <winaldodwiputra@gmail.com>
2022-10-06 00:29:56 +0700

    Create study.php

---
+++ study.php
@@ -0,0 +1,44 @@
+ <section id="resume" class="resume">
+     <div class="container">
+
+         <div class="section-title">
+             <h2>Study</h2>
+             <p>Berikut adalah rekam jejak study saya dari awal saya bersekolah hingga saat ini</p>
+         </div>
+
+         <div class="row">
+             <div class="col-lg-6" data-aos="fade-up">
+                 <div class="resume-item">
+                     <h4>Sekolah Dasar</h4>
+                     <h5>2007 - 2013</h5>
+                     <p>SDN 9 Sigi, Sulawesi tengah</p>
+                 </div>
+                 <div class="resume-item">
+                 <h4>Sekolah Menengah Pertama</h4>
+                     <h5>2013 - 2016</h5>
+                     <p>SMPN 3 Sigi, Sulawesi tengah</p>
+                 </div>
+                 <div class="resume-item">
+                 <h4>Sekolah Menengah Atas</h4>
+                     <h5>2016 - 2019</h5>
+                     <p>SMAN 20 Jakarta, DKI Jakarta</p>
+                 </div>
+             </div>
+
+             <div class="col-lg-6" data-aos="fade-up" data-aos-delay="100">
+                
+                 <div class="resume-item">
+                     <h4>Kuliah</h4>
+                     <h5>2019 - Now</h5>
+                     <p><em>Universitas Negeri Malang</em></p>
+                     <ul>
+                         <li>Teknik Informatika</li>
+                         <li>Semester 7</li>
+                     </ul>
+                 </div>
+                 
+             </div>
+         </div>
+
+     </div>
+ </section>
